47488559586cc57a2b2de38ae11c4788bd16cfb4
Anton <ansorokey@gmail.com>
2022-10-27 19:54:31 -0400

    added strings exercise

---
+++ strings-exercise.js
@@ -0,0 +1,10 @@
+// Strings Exercise
+// Write 3 examples of expressions.
+// One using + (concatenation), another using [] (indexing), and another using .length.
+// Print out the result of your expressions using console.log.
+// Be sure to test your work by running your code!
+
+// Example:
+console.log('hello ' + 'world');  // 'hello world'
+
+// Write 3 more expressions below:
