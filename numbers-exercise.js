1ee6fada360b8bdca8877ec9dd92d3b000346346
Anton <ansorokey@gmail.com>
2022-10-27 19:49:50 -0400

    added numbers exercise

---
+++ numbers-exercise.js
@@ -0,0 +1,8 @@
+// Numbers Exercise
+// Write 5 examples of expressions using each of the operations: +, -, *, /, and %.
+// Print out the result of your expressions using console.log. Be sure to test your work by running your code!
+
+// Example:
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
