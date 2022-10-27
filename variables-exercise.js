999f7540b99415a16b74217bc7e3e53d360d566d
Anton <ansorokey@gmail.com>
2022-10-27 19:52:15 -0400

    added variables exercise

---
+++ variables-exercise.js
@@ -0,0 +1,21 @@
+// Variables Exercise
+// Time to practice variables! Follow the comments for instructions. Be sure to test your work by running your code!
+
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+
+// 3. Declare a variable called `age`:
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+// 5. Assign the `age` variable to a number:
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
