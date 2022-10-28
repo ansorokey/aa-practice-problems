c9f39ff890e1ec3a39a9632486645b0a4a590edb
Anton <ansorokey@gmail.com>
2022-10-27 20:28:50 -0400

    added fizz buzz

---
+++ fizz-buzz.js
@@ -0,0 +1,16 @@
+// Fizz Buzz
+//  Define a function fizzBuzz(max) that takes a number and prints
+// every number from 0 to max (not inclusive) that is divisible by either 3 or 5, but not
+// both.
+
+
+// Examples:
+
+fizzBuzz(20); // prints out:
+// 3
+// 5
+// 6
+// 9
+// 10
+// 12
+// 18
