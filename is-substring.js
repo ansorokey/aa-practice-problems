3f115fb157e6fbc5fdd458d03bc4cd6315d6d466
Anton <ansorokey@gmail.com>
2022-10-27 20:27:43 -0400

    added is substring

---
+++ is-substring.js
@@ -0,0 +1,13 @@
+// Is Substring
+// In JavaScript the String.indexOf() method allows you to get the index of a value within an Array-like structure, including strings.
+
+// Write a function isSubstring that takes in two strings, searchString and subString.
+// The function should return true if subString is a part of thesearchString, regardless of upper or lower case, and false if otherwise.
+
+function isSubstring(searchString, subString) {
+    // your code here...
+  }
+
+  console.log(isSubstring("The cat went to the store", "he cat went")); // => true
+  console.log(isSubstring("Time to program", "time")); // => true
+  console.log(isSubstring("Jump for joy", "joys")); // => false
