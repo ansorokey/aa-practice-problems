ea550ff94fcfa1749af2765853a614ceee470295
Anton <ansorokey@gmail.com>
2022-10-29 12:08:21 -0400

    added reverse string

---
+++ reverse-string.js
@@ -0,0 +1,11 @@
+// Reverse String
+// Write a function reverseString(str) that takes in a string.
+// The function should return a new string where the order of the characters is reversed.
+
+// your code here
+
+
+
+
+console.log(reverseString('fish')); // 'hsif'
+console.log(reverseString('marathon')); // 'nohtaram'
