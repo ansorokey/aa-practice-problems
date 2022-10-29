43a73bceed09d5336c92f5ed31dc20eaa6ebcac0
Anton <ansorokey@gmail.com>
2022-10-29 12:19:32 -0400

    added first vowel

---
+++ first-vowel.js
@@ -0,0 +1,16 @@
+// First Vowel
+// Write a function firstVowel(str) that takes in a string and returns the first vowel that appears sequentially in the string.
+// If the string does not contain a vowel, return null.
+
+// your code here
+
+
+
+
+
+
+console.log(firstVowel('battery')); // 'a'
+console.log(firstVowel('tunnel')); // 'u'
+console.log(firstVowel('dog')); // 'o'
+console.log(firstVowel('conventional')); // 'o'
+console.log(firstVowel('rhythm')); // null
