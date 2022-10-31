3e21fa05f2be20a56c5e416d1d74b93510aeb0b6
Anton <ansorokey@gmail.com>
2022-10-31 19:23:26 -0400

    added prime factors

---
+++ prime-factors.js
@@ -0,0 +1,9 @@
+// Prime Factors
+// Write a function primeFactors that takes in a number and returns an array containing all of the prime factors of the given number.
+
+function primeFactors(n) {
+    // your code here
+}
+
+console.log(primeFactors(24)); // [2, 3]
+console.log(primeFactors(60)); // [2, 3, 5]
