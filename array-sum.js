cdf3860d44c902a2a502ed689dafdc25d5d85d7f
Anton <ansorokey@gmail.com>
2022-10-31 19:48:53 -0400

    added array sum

---
+++ array-sum.js
@@ -0,0 +1,15 @@
+// Array Sum
+// Write a function sumArray(arr) that accepts an array as an arg.
+// The function should return the total sum of all values in the array.
+
+// your code here
+
+
+
+
+
+
+
+console.log(sumArray([4, 3, -1, 10])); // 16
+console.log(sumArray([6, 7, 2])); // 15
+console.log(sumArray([])); // 0
