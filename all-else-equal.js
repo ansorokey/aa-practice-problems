f254aa4d1d6a90821b4b12565e4ab297dc1a093a
Anton <ansorokey@gmail.com>
2022-10-31 19:40:08 -0400

    added pyramid sum

---
+++ all-else-equal.js
@@ -0,0 +1,12 @@
+// All Else Equal
+// Write a function allElseEqual that takes in an array of numbers.
+// The function should return the element of the array that is equal to half of the sum of all elements of the array.
+// If there is no such element, the method should return null.
+
+function allElseEqual(arr) {
+    // your code here
+}
+
+console.log(allElseEqual([2, 4, 3, 10, 1])); // 10
+console.log(allElseEqual([6, 3, 5, -9, 1])); // 3
+console.log(allElseEqual([1, 2, 3, 4]));     // null
