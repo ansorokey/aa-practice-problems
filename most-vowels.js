8c902a2c9d3126af18a1fd69d440a7ad6e446ba1
Anton <ansorokey@gmail.com>
2022-10-31 19:21:07 -0400

    added most vowels

---
+++ most-vowels.js
@@ -0,0 +1,8 @@
+// Most Vowels
+// Write a function mostVowels that takes in a sentence string and returns the word of the sentence that contains the most vowels.
+
+function mostVowels(sentence) {
+    // your code here
+}
+
+console.log(mostVowels("what a wonderful life")); // "wonderful"
