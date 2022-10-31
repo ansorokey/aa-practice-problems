6e38edf001b047d37a4a22798d136cfc7b0e5bb8
Anton <ansorokey@gmail.com>
2022-10-31 19:24:57 -0400

    added greatest factor array

---
+++ greatest-factor-array.js
@@ -0,0 +1,12 @@
+// Greatest Factor Array
+// Write a function greatestFactorArray that takes in an array of numbers and returns a new array where every even number is replaced with it's greatest factor.
+// A greatest factor is the largest number that divides another with no remainder.
+// For example, the greatest factor of 16 is 8.
+// (For the purpose of this problem we won't say the greatest factor of 16 is 16, because that would be too easy)
+
+function greatestFactorArray(array) {
+    // your code here
+}
+
+console.log(greatestFactorArray([16, 7, 9, 14])); // [8, 7, 9, 7]
+console.log(greatestFactorArray([30, 3, 24, 21, 10])); // [15, 3, 12, 21, 5]
