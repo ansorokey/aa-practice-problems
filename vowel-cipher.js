9f814127e0b35b4c159e8e93d60437130f8135f4
Anton <ansorokey@gmail.com>
2022-10-31 19:34:47 -0400

    added vowel cipher

---
+++ vowel-cipher.js
@@ -0,0 +1,11 @@
+// Vowel Cipher
+// Write a function vowelCipher that takes in a string and returns a new string where every vowel becomes the next vowel in the alphabet.
+
+
+function vowelCipher(string) {
+    // your code here
+}
+
+
+console.log(vowelCipher("bootcamp")); // "buutcemp"
+console.log(vowelCipher("paper cup")); // "pepir cap"
