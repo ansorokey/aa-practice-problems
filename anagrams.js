e51fb68180d65b2e307c4518c3efd27577940e95
Anton <ansorokey@gmail.com>
2022-10-31 19:44:15 -0400

    added anagrams

---
+++ anagrams.js
@@ -0,0 +1,13 @@
+// Anagrams
+// Write a function anagrams that takes in two words and returns a boolean indicating whether or not the words are anagrams.
+// Anagrams are words that contain the same characters but not necessarily in the same order.
+// Solve this without using .sort().
+
+function anagrams(word1, word2) {
+    // your code here
+}
+
+console.log(anagrams("cat", "act"));          // true
+console.log(anagrams("restful", "fluster"));  // true
+console.log(anagrams("cat", "dog"));          // false
+console.log(anagrams("boot", "bootcamp"));    // false
