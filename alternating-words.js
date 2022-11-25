af637bc64b424a3f2d463e6f747a3e62d7e5bf8f
justlaurnz <laurnz.p@outlook.com>
2022-11-25 03:59:26 -0800

    pushing through week two

---
+++ alternating-words.js
@@ -0,0 +1,16 @@
+// Write a function alternatingWords that accepts an array of
+// words as an argument. The function should mutate the input
+// array such that the words alternate between fully uppercase
+// or lowercase. The first word should be uppercase.
+
+/* *goodluck!* */
+
+
+
+let words1 = ['Belka', 'STRELKA', 'laika', 'DEZIK', 'Tsygan'];
+alternatingWords(words1);
+console.log(words1); // [ 'BELKA', 'strelka', 'LAIKA', 'dezik', 'TSYGAN' ]
+
+let words2 = ['Yellowstone', 'Yosemite', 'Zion', 'Acadia'];
+alternatingWords(words2);
+console.log(words2); // [ 'YELLOWSTONE', 'yosemite', 'ZION', 'acadia' ]
---
+++ hipsterfy.js
@@ -0,0 +1,10 @@
+// Write a function hipsterfy(sentence) that takes in a
+// sentence string and returns the sentence where every
+// word is missing its last vowel.
+
+/* *goodluck* */
+
+
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
