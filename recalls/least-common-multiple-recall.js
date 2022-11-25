87ff16c8980216c737b85e9226ac1acd12958300
justlaurnz <laurnz.p@outlook.com>
2022-11-25 04:51:40 -0800

    hello again, recalls folder added

---
+++ recalls/least-common-multiple-recall.js
@@ -0,0 +1,15 @@
+// Write a function leastCommonMultiple
+// (num1, num2) that accepts two numbers
+// as arguments. The functions should
+// return the smallest number that is
+// divisible by both num1 and num2.
+
+
+
+let leastCommonMultiple = function(num1, num2){
+
+};
+
+console.log(leastCommonMultiple(6, 4)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
---
+++ recalls/string-exercise-recall.js
@@ -0,0 +1,5 @@
+// * Write 3 examples of expressions.
+//One using + (concatenation), another using [] (indexing),
+// and another using .length. Print out the result of your
+// expressions using console.log. Be sure to test your work by
+// running your code!
