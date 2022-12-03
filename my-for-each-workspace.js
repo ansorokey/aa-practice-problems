501df03b0010175672c9533d280d16d4d9c73c51
justlaurnz <justlaurnz@gmail.com>
2022-12-02 23:25:32 -0800

    start adding week 4

---
+++ my-for-each-workspace.js
@@ -0,0 +1,5 @@
+// Use this REPL to code along with the My For Each Demo,
+// or try implementing it yourself.
+const myForEach = (arr, cb) => {
+
+}
---
+++ my-map-workspace.js
@@ -0,0 +1,5 @@
+// Use this REPL to code along with the My Map Demo,
+// or try implementing it yourself.
+const myMap = (arr, cb) => {
+
+}
