a3f4d5262b5e656b0f24933745beb579f559a4bd
justlaurnz <justlaurnz@gmail.com>
2022-12-03 01:12:01 -0800

    added w2 sat up to shortest word, silly strings was last

---
+++ triplet-true.js
@@ -0,0 +1,14 @@
+// Write a function tripletTrue that accepts a string as an argument.
+// The function should return a boolean indicating whether or not the
+// the string contains three of the same character consecutively.
+
+
+// Your code here
+
+
+
+console.log(tripletTrue('caaabb'));        // true
+console.log(tripletTrue('terrrrrible'));   // true
+console.log(tripletTrue('runninggg'));     // true
+console.log(tripletTrue('bootcamp'));      // false
+console.log(tripletTrue('e'));             // false
